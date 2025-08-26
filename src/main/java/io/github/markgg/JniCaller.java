c5cbc1a329b85ba4d79c7715c3e8fd6a983447b8
desync_lord <86848614+MarkGG8181@users.noreply.github.com>
2025-08-26 19:51:24 +0300

    natives + jni caller

---
+++ src/main/java/io/github/markgg/JniCaller.java
@@ -0,0 +1,41 @@
+package io.github.markgg;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.lang.reflect.Constructor;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class JniCaller {
+    public static final Logger LOGGER = LoggerFactory.getLogger("JniCaller");
+
+    private final ClassLoader loader;
+
+    public JniCaller(File jarFile) throws Exception {
+        if (!jarFile.exists()) {
+            LOGGER.error("Obfuscated Aegis jar is nowhere to be found - {}", jarFile.getAbsolutePath());
+            throw new RuntimeException("fuckass");
+        }
+
+        this.loader = new URLClassLoader(new URL[]{jarFile.toURL()}, getClass().getClassLoader());
+    }
+
+    public Object load(String className) {
+        try {
+            Class<?> clazz = Class.forName(className.replace('/', '.'), true, loader);
+
+            Constructor<?> ctor = clazz.getDeclaredConstructor();
+            ctor.setAccessible(true);
+
+            Object instance = ctor.newInstance();
+
+            LOGGER.error("Class with the name {} was found", className);
+
+            return instance;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
